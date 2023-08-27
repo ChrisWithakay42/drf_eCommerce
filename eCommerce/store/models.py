0061116757e1922a5abf4ed39b474af5bff89db5
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-27 09:52:31 +0100

    init

--- eCommerce/store/models.py
+++ eCommerce/store/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 
+
+class Product(models.Model):
+    id_ = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=350, blank=False)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    # image = models.ImageField(path=)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    ...
